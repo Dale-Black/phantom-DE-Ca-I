0fbf1eb309ec17917c8c98bfc6e28c3c0ce4aeea
Dale <djblack@uci.edu>
2021-08-18 20:19:49 -0700

    update

--- notebooks/calibration_water_iodine_calcium.jl
+++ notebooks/calibration_water_iodine_calcium.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.4
+# v0.15.1
 
 using Markdown
 using InteractiveUtils
@@ -68,23 +68,10 @@ md"""
 filepath = "/Users/daleblack/Google Drive/dev/julia/research/phantom-DE-Ca-I/data/calibration_water_iodine_calcium.csv";
 
 # ╔═╡ be1ca5ec-db9a-48ee-9236-3630f74e7bfb
-begin
-	df = DataFrame(CSV.File(filepath))
-	# Remove first row with only water measurements
-	delete!(df, 1)
-end
+df = DataFrame(CSV.File(filepath))
 
-# ╔═╡ 8c483d7a-5a69-4680-bbca-cdc9600c3ace
-begin
-	df_iodine = df[1:7, :]
-	df_calcium = df[8:end, :];
-end
-
-# ╔═╡ 687e4bd5-e3ad-4b73-98c1-fcd79bcbd3f3
-begin
-	train_df_i, test_df_i = MLDataUtils.splitobs(MLDataUtils.shuffleobs(df_iodine), at = 0.8)
-	train_df_ca, test_df_ca = MLDataUtils.splitobs(MLDataUtils.shuffleobs(df_calcium), at = 0.8)
-end
+# ╔═╡ bc30c569-138e-46a3-8602-1788191ec936
+train_df, test_df = splitobs(shuffleobs(df), at = 0.8)
 
 # ╔═╡ 4689b9a5-5f6a-4333-a76e-8f0a25a2e5b3
 md"""
@@ -92,21 +79,19 @@ md"""
 """
 
 # ╔═╡ a696a524-b9ce-4ab3-a564-027ddafdc7b7
-@. multimodel(x, p) = (p[1] + (p[2] * x[:, 1]) + (p[3] * x[:, 2]) + (p[4] * x[:, 1]^2) + (p[5] * x[:, 1] * x[:, 2]) + (p[6] * x[:, 2]^2)) / (1 + (p[7] * x[:, 1]) + (p[8] * x[:, 2]))
+multimodel(x, p) = (p[1] .+ (p[2] .* x[:, 1]) .+ (p[3] .* x[:, 2]) .+ (p[4] .* x[:, 1].^2) .+ (p[5] .* x[:, 1] .* x[:, 2]) .+ (p[6] .* x[:, 2].^2)) ./ (1 .+ (p[7] .* x[:, 1]) + (p[8] .* x[:, 2]))
 
-# ╔═╡ d4cace91-6695-4d40-ade8-37e7a7014bc4
-begin
-	xdata_i = hcat(train_df_i[!, :low_energy], train_df_i[!, :high_energy])
-	xdata_ca = hcat(train_df_ca[!, :low_energy], train_df_ca[!, :high_energy])
-	p0 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-end;
+# ╔═╡ 5342b070-8c9b-4bcd-a06a-18cbdb57c1ff
+xdata = hcat(train_df[!, :low_energy], train_df[!, :high_energy]);
 
-# ╔═╡ addca25f-d566-4ff1-bf34-e687bd4840e7
-begin
-	fit_iodine = LsqFit.curve_fit(multimodel, xdata_i, train_df_i[!, :iodine], p0).param
-	
-	fit_calcium = LsqFit.curve_fit(multimodel, xdata_ca, train_df_ca[!, :calcium], p0).param
-end;
+# ╔═╡ 3ac4340f-1497-4ebe-8015-f6e2e728c655
+p0 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+
+# ╔═╡ a1041244-5856-4d81-9b3c-cc66e8a64f43
+fit_all_ca = LsqFit.curve_fit(multimodel, xdata, train_df[!, :calcium], p0).param
+
+# ╔═╡ e8199f02-554d-473e-9d0e-80cd1acfaec4
+fit_all_i = LsqFit.curve_fit(multimodel, xdata, train_df[!, :iodine], p0).param
 
 # ╔═╡ 7553f6b0-4f11-42fd-93ee-e96bddaaabce
 md"""
@@ -114,7 +99,7 @@ md"""
 """
 
 # ╔═╡ 60b8894f-ba15-4cae-8e78-a81e00606399
-params_df = DataFrame("fit_iodine" => fit_iodine, "fit_calcium" => fit_calcium)
+params_df = DataFrame("fit_iodine" => fit_all_i, "fit_calcium" => fit_all_ca)
 
 # ╔═╡ 4a3b964b-d26e-4226-9b68-30a95e7f06fc
 CSV.write("/Users/daleblack/Google Drive/dev/julia/research/phantom-DE-Ca-I/data/params_iodine_calcium1.csv", params_df);
@@ -131,16 +116,16 @@ function predict_concentration(x, y, p)
 	F = A / B
 end
 
-# ╔═╡ 91b6b97e-3575-42b7-b921-34d2859d214c
+# ╔═╡ cdc842d3-d692-4ac9-8f75-1aa67109a65b
 begin
-	arr_iodine = Array{Float64}(undef, nrow(test_df_i))
-	for i in 1:nrow(test_df_i)
-		arr_iodine[i] =  predict_concentration(test_df_i[!, :low_energy][i], test_df_i[!, :high_energy][i], fit_iodine)
+	all_arr_iodine = Array{Float64}(undef, nrow(test_df))
+	for i in 1:nrow(test_df)
+		all_arr_iodine[i] =  predict_concentration(test_df[!, :low_energy][i], test_df[!, :high_energy][i], fit_all_i)
 	end
 	
-	arr_calcium = Array{Float64}(undef, nrow(test_df_ca))
-	for i in 1:nrow(test_df_ca)
-		arr_calcium[i] =  predict_concentration(test_df_ca[!, :low_energy][i], test_df_ca[!, :high_energy][i], fit_calcium)
+	all_arr_calcium = Array{Float64}(undef, nrow(test_df))
+	for i in 1:nrow(test_df)
+		all_arr_calcium[i] =  predict_concentration(test_df[!, :low_energy][i], test_df[!, :high_energy][i], fit_all_ca)
 	end
 end
 
@@ -149,17 +134,27 @@ md"""
 Double check that the held out iodine and calcium concentrations seem reasonable by using the calibrated equation. Once calibrated, the equation can predict the concentration given two intensity measurements `low_energy` and `high_energy`
 """
 
-# ╔═╡ 2ccacff3-eee3-45da-8e7c-0689871442d5
-test_df_i
+# ╔═╡ 903edf44-e444-4f2d-a61c-15f2decf890a
+md"""
+### Iodine results
+"""
 
-# ╔═╡ fbf5b3cd-ac99-4154-a289-941bb7e5030a
-arr_iodine
+# ╔═╡ 875e06ce-012a-43e6-90bc-5558190ac66b
+test_df
 
-# ╔═╡ d46453a2-3e64-4536-8349-dbd319789955
-test_df_ca
+# ╔═╡ 48722c03-0ad8-4e03-a44e-2d156cb853aa
+all_arr_iodine
 
-# ╔═╡ 5e53d942-64ad-4519-aaa4-cdbc53c3cf44
-arr_calcium
+# ╔═╡ 7dbd984c-307c-41d8-b260-f6e0158b14bc
+md"""
+### Calcium results
+"""
+
+# ╔═╡ bf048d05-e491-4b94-82d7-387536bc433a
+test_df
+
+# ╔═╡ 63a28676-a73a-4088-9969-536f814be3e7
+all_arr_calcium
 
 # ╔═╡ Cell order:
 # ╠═f2fa7d36-9b26-11eb-00e1-3b8d81722ef9
@@ -168,20 +163,23 @@ arr_calcium
 # ╟─3e506496-ef3e-4f1e-8968-fb4ed4a50746
 # ╠═f12f7207-66d2-46b7-9d18-724b6ea11ca5
 # ╠═be1ca5ec-db9a-48ee-9236-3630f74e7bfb
-# ╠═8c483d7a-5a69-4680-bbca-cdc9600c3ace
-# ╠═687e4bd5-e3ad-4b73-98c1-fcd79bcbd3f3
+# ╠═bc30c569-138e-46a3-8602-1788191ec936
 # ╟─4689b9a5-5f6a-4333-a76e-8f0a25a2e5b3
 # ╠═a696a524-b9ce-4ab3-a564-027ddafdc7b7
-# ╠═d4cace91-6695-4d40-ade8-37e7a7014bc4
-# ╠═addca25f-d566-4ff1-bf34-e687bd4840e7
+# ╠═5342b070-8c9b-4bcd-a06a-18cbdb57c1ff
+# ╠═3ac4340f-1497-4ebe-8015-f6e2e728c655
+# ╠═a1041244-5856-4d81-9b3c-cc66e8a64f43
+# ╠═e8199f02-554d-473e-9d0e-80cd1acfaec4
 # ╟─7553f6b0-4f11-42fd-93ee-e96bddaaabce
 # ╠═60b8894f-ba15-4cae-8e78-a81e00606399
 # ╠═4a3b964b-d26e-4226-9b68-30a95e7f06fc
 # ╟─7d851c3f-9f63-45c0-8c1a-ad5a1fef329f
 # ╠═eb4a0792-31ba-480a-af99-421837fee8fd
-# ╠═91b6b97e-3575-42b7-b921-34d2859d214c
+# ╠═cdc842d3-d692-4ac9-8f75-1aa67109a65b
 # ╟─8616b7a0-39a4-4899-ab0c-13ef53d3a3d2
-# ╠═2ccacff3-eee3-45da-8e7c-0689871442d5
-# ╠═fbf5b3cd-ac99-4154-a289-941bb7e5030a
-# ╠═d46453a2-3e64-4536-8349-dbd319789955
-# ╠═5e53d942-64ad-4519-aaa4-cdbc53c3cf44
+# ╟─903edf44-e444-4f2d-a61c-15f2decf890a
+# ╠═875e06ce-012a-43e6-90bc-5558190ac66b
+# ╠═48722c03-0ad8-4e03-a44e-2d156cb853aa
+# ╟─7dbd984c-307c-41d8-b260-f6e0158b14bc
+# ╠═bf048d05-e491-4b94-82d7-387536bc433a
+# ╠═63a28676-a73a-4088-9969-536f814be3e7
